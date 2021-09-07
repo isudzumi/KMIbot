899d1282eb2374e8219496b74c3520c0d6adcb8b
Hiroaki ITO <mail@isudzumi.com>
2021-09-07 20:36:50 +0900

    Rewrite with WebhookHandler

--- app.py
+++ app.py
@@ -2,22 +2,27 @@ import os
 import json
 import logging
 from linebot import (
-    LineBotApi, WebhookParser
+    LineBotApi,  WebhookHandler
 )
 from linebot.models import (
     TextSendMessage, MessageEvent, TextMessage
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError,
+    LineBotApiError
 )
 import transform
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 LINE_ACCESS_TOKEN=os.environ['LINE_ACCESS_TOKEN']
 LINE_CHANNEL_SECRET=os.environ['LINE_CHANNEL_SECRET']
 MAGIC_KEYWORD=os.environ['MAGIC_KEYWORD']
 
 line_bot_api = LineBotApi(LINE_ACCESS_TOKEN)
-parser = WebhookParser(LINE_CHANNEL_SECRET)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
 
 def create_lambda_response(statusCode: int):
     """ Create output for Lambda Proxy Integration
@@ -35,37 +40,43 @@ def create_lambda_response(statusCode: int):
         'body': '{}'
     }
 
-def handler(event, context):
+
+@handler.add(MessageEvent, message=TextMessage)
+def reply_message(line_event):
+    text = line_event.message.text
+    if (not text.startswith(MAGIC_KEYWORD)):
+        return
+    input_text = text.removeprefix(MAGIC_KEYWORD)
+    if (not input_text):
+        return
+    logger.info(input_text)
+    generated_text = transform.generate(input_text)
+    logger.info(generated_text)
+    line_bot_api.reply_message(
+        line_event.reply_token,
+        TextSendMessage(text=generated_text)
+    )
+
+
+def lambda_handler(event, context):
     try:
         signature = event['headers'].get('x-line-signature')
-        logging.info(signature)
+        logger.info(signature)
         if (not signature):
             return create_lambda_response(403)
-        events = parser.parse(json.dumps(event['body']), signature)
-        
-        for event in events:
-            if not isinstance(event, MessageEvent):
-                continue
-            if not isinstance(event.message, TextMessage):
-                continue
-            message = event.message.text
-            if (not message.startswith(MAGIC_KEYWORD)):
-                continue
-            input_text = message.removeprefix(MAGIC_KEYWORD)
-            if (not input_text):
-                continue
-            logging.info(input_text)
-            text = transform.generate(input_text)
-            logging.info(text)
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=text)
-            )
+        body = json.dumps(event['body'])
+
+        handler.handle(body, signature)
 
         return create_lambda_response(200)
     except InvalidSignatureError:
-        logging.error('Invalid Signature Error')
+        logger.error('Invalid Signature Error')
         return create_lambda_response(400)
+    except LineBotApiError as e:
+        for m in e.error.details:
+            logger.error(f'LINE Bot API Error: {m.property} {m.message}')
+        return create_lambda_response(500)
+
 
 if __name__ == '__main__':
     pass
